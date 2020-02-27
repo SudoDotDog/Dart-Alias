192ff961c336568fa35eb28268530ae8c9f477f2
WMXPY <wm@mengw.io>
2020-02-27 10:49:22 -0600

    add alias map

--- lib/src/alias-map.dart
+++ lib/src/alias-map.dart
@@ -1,5 +1,20 @@
+import 'package:english_words/english_words.dart';
+
 class AliasMap {
   final Map<String, String> _map = {};
 
   AliasMap() {}
+
+  String getAlias(String identifier) {
+    if (this._map[identifier] != null) {
+      return this._map[identifier];
+    }
+
+    final WordPair wordPair = WordPair.random();
+    final String value = wordPair.asPascalCase;
+
+    this._map[identifier] = value;
+
+    return value;
+  }
 }
